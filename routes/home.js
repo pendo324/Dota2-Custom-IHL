5de0e6d459da219ad5b85ed2af286866b7cf81e8
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-02 12:31:22 +0800

    Delete routes directory

--- routes/home.js
+++
@@ -1,9 +0,0 @@
-var express = require("express"),
-	path = require("path"),
-	app = express.Router();
-
-app.route("/").get(function(req, res) {
-    res.sendFile(path.join(__dirname + "/../html/index.html"));
-});
-
-module.exports = app;
